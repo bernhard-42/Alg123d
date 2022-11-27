08c42550a1bd04b9945b37a77c609ec91b347d44
Bernhard <b_walter@arcor.de>
2022-11-27 12:53:01 +0100

    moved to @ for position/tangent access

--- examples/canadian_flag.py
+++ examples/canadian_flag.py
@@ -12,12 +12,12 @@ l5 = Polyline(((0.2469, 0.6267), (0.225, 0.6781), (0.1369, 0.5835)))
 l6 = Polyline(((0.1138, 0.5954), (0.1562, 0.8146), (0.0881, 0.7752)))
 l7 = Line((0.0692, 0.7808), (0.0000, 0.9167))
 
-r1 = RadiusArc(l1(1), l2(0), 0.0271)
-r2 = TangentArc(l2(1), l3(0), tangent=l2 % 1)
-r3 = SagittaArc(l3(1), l4(0), 0.003)
-r4 = ThreePointArc((l4(1), (l4(1) + l5(0)) * 0.5 + Vector(-0.002, -0.002), l5(0)))
-r5 = TangentArc(l6(1), l7(0), tangent=l6 % 1)
-s = Spline((l5(1), l6(0)), tangents=(l5 % 1, l6 % 0), tangent_scalars=(2, 2))
+r1 = RadiusArc(l1 @ 1, l2 @ 0, 0.0271)
+r2 = TangentArc(l2 @ 1, l3 @ 0, tangent=l2 % 1)
+r3 = SagittaArc(l3 @ 1, l4 @ 0, 0.003)
+r4 = ThreePointArc((l4 @ 1, (l4 @ 1 + l5 @ 0) * 0.5 + Vector(-0.002, -0.002), l5 @ 0))
+r5 = TangentArc(l6 @ 1, l7 @ 0, tangent=l6 % 1)
+s = Spline((l5 @ 1, l6 @ 0), tangents=(l5 % 1, l6 % 0), tangent_scalars=(2, 2))
 
 flag = l1 + l2 + l3 + l4 + l5 + l6 + l7 + r1 + r2 + r3 + r4 + r5 + s
 flag += mirror(flag, about=Plane.YZ)
