d3f09d35c50fd279d217bad5e1d807a15bdc031a
Bernhard <b_walter@arcor.de>
2022-11-28 22:16:14 +0100

    some clean up

--- main.py
+++ main.py
@@ -16,15 +16,15 @@ box = Box(0.3, 0.3, 0.5)
 
 # %%
 
-# p = cyl @ plane
+p = cyl @ plane
 
-# for loc in PolarLocations(0.7, 10):
-#     p -= box @ (plane * loc)
+for loc in PolarLocations(0.7, 10):
+    p -= box @ (plane * loc)
 
-# show(p)
-# # %%
+show(p)
+# %%
 
-# show(p, p.faces().group_by(Axis.Y)[0], transparent=True)
+show(p, p.faces().group_by(Axis.Y)[0], transparent=True)
 
 # %%
 
@@ -32,15 +32,11 @@ locs = [Location((0, 0, 0), (0, a, 0)) for a in (0, 45, 90, 135)]
 
 s = Empty3()
 for i, outer_loc in enumerate(GridLocations(3, 3, 2, 2)):
-    c_plane = (
-        plane * outer_loc * locs[i]
-    )  # on plane, located to grid positions, and finally rotated
+    c_plane = plane * outer_loc * locs[i]
     s += Circle(1) @ c_plane
 
     for loc in PolarLocations(0.8, (i + 3) * 2):
-        s -= Rectangle(0.1, 0.3) @ (c_plane * loc)  # Use polar locations on c_plane
+        s -= Rectangle(0.1, 0.3) @ (c_plane * loc * Rotation(0, 0, 45))
 
 e = extrude(s, 0.3)
-show(e)
-
-# %%
+show(e, reset_camera=False)
