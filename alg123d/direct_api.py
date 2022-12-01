aa54363b967bb1b83f75b8541db186abb772c304
Bernhard <b_walter@arcor.de>
2022-12-01 22:06:57 +0100

    removed Plane patches

--- alg123d/direct_api.py
+++ alg123d/direct_api.py
@@ -16,39 +16,9 @@ def tupleize(arg):
         return (arg,)
 
 
-def as_plane(obj):
-    if isinstance(obj, (Face, Location)):
-        if isinstance(obj, Face):
-            face = obj
-            origin = face.center()
-        else:
-            face = Face.make_rect(1, 1).move(obj)
-            origin = obj.position
-
-        x_dir = Vector(face._geom_adaptor().Position().XDirection())
-        z_dir = face.normal_at(origin)
-
-        return Plane(origin=origin, x_dir=x_dir, z_dir=z_dir)
-
-    elif isinstance(obj, Compound) and hasattr(obj, "location"):
-        return as_plane(obj.location)
-
-
-def as_planes(objs):
-    return [as_plane(obj) for obj in objs]
-
-
-def __plane_mul__(self, loc: Location) -> Plane:
-    if not isinstance(loc, Location):
-        raise RuntimeError(
-            "Planes can only be multiplied with Locations to relocate them"
-        )
-    return as_plane(self.to_location() * loc)
-
-
 def __neg__axis__(self):
     return self.reverse()
 
 
-Plane.__mul__ = __plane_mul__
+# Plane.__mul__ = __plane_mul__
 Axis.__neg__ = __neg__axis__
