60d7ece406a422c97fadac205303a09f32241587
Bernhard <b_walter@arcor.de>
2022-12-04 12:16:49 +0100

    further examples ported

---
+++ examples/holes.py
@@ -0,0 +1,30 @@
+from alg123d import *
+import alg123d.shortcuts as S
+
+thru_hole = Cylinder(radius=3, height=2)
+thru_hole -= Bore(thru_hole, radius=1)
+
+# Recessed counter bore hole (hole location @ (0,0,0))
+recessed_counter_bore = Cylinder(radius=3, height=2)
+recessed_counter_bore -= CounterBore(
+    recessed_counter_bore, radius=1, counter_bore_radius=1.5, counter_bore_depth=0.5
+)
+
+# Recessed counter sink hole (hole location @ (0,0,0))
+recessed_counter_sink = Cylinder(radius=3, height=2)
+recessed_counter_sink -= CounterSink(
+    recessed_counter_sink, radius=1, counter_sink_radius=1.5
+)
+
+# Flush counter sink hole (hole location @ (0,0,2))
+flush_counter_sink = Cylinder(radius=3, height=2)
+plane = Plane(S.max_face(flush_counter_sink))
+flush_counter_sink -= (
+    CounterSink(flush_counter_sink, radius=1, counter_sink_radius=1.5) @ plane
+)
+
+if "show_object" in locals():
+    show_object(thru_hole, name="though hole")
+    show_object(recessed_counter_bore @ (10, 0), name="recessed counter bore")
+    show_object(recessed_counter_sink @ (0, 10), name="recessed counter sink")
+    show_object(flush_counter_sink @ (10, 10), name="flush counter sink")
---
+++ examples/intersecting_chamfers.py
@@ -0,0 +1,15 @@
+from alg123d import *
+
+blocks = Box(1, 2, 1, centered=(True, False, False)) @ (-1, -1, 0)
+blocks += Box(1, 1, 2, centered=(True, False, False))
+blocks += Box(1, 2, 1, centered=(True, False, False)) @ (1, -1, 0)
+
+bottom_edges = blocks.edges().filter_by_position(Axis.Z, 0, 1, inclusive=(True, False))
+blocks2 = chamfer(blocks, bottom_edges, length=0.1)
+
+top_edges = blocks2.edges().filter_by_position(Axis.Z, 1, 2, inclusive=(False, True))
+blocks2 = chamfer(blocks2, top_edges, length=0.1)
+
+
+if "show_object" in locals():
+    show_object(blocks2)
