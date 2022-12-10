9093df1f6c760215dddc46870924b55de34a9d6c
Bernhard <b_walter@arcor.de>
2022-12-10 07:59:07 +0100

    added cq and b123d conversion shortcuts

--- alg123d/shortcuts.py
+++ alg123d/shortcuts.py
@@ -110,3 +110,34 @@ def min_vertices(a: Compound, axis: Axis = Axis.Z) -> ShapeList:
 
 def max_vertices(a: Compound, axis: Axis = Axis.Z) -> ShapeList:
     return a.vertices().group_by(axis)[-1]
+
+
+def from_cq(obj):
+    return AlgCompound.make_compound(obj.objects)
+
+
+def to_cq(obj):
+    import cadquery as cq
+
+    return cq.Workplane().newObject([cq.Shape.cast(o.wrapped) for o in obj])
+
+
+def from_bd(obj):
+    if hasattr(obj, "_obj_name"):
+        return AlgCompound(getattr(obj, obj._obj_name))
+    else:
+        return obj
+
+
+def to_bd(obj):
+    import build123d as bd
+
+    if hasattr(obj, "dim"):
+        ctx = {1: bd.BuildLine, 2: bd.BuildSketch, 3: bd.BuildPart}
+        with ctx[obj.dim]() as c:
+            bd.Add(Compound.make_compound(list(obj)))
+        return c
+    else:
+        return bd.ShapeList(
+            bd.Compound.make_compound([bd.Shape.cast(o.wrapped) for o in obj])
+        )
