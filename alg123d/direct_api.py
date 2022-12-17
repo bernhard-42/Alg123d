b1b869c4dc743d7d39de87e0a164fc8e6a588595
Bernhard <b_walter@arcor.de>
2022-12-17 17:35:28 +0100

    correct face location algorithm

--- alg123d/direct_api.py
+++ alg123d/direct_api.py
@@ -1,2 +1,12 @@
 from build123d.direct_api import *
 from build123d.build_enums import *
+
+
+def _face_location(self):
+    origin = self.center()
+    x_dir = Vector(self._geom_adaptor().Position().XDirection())
+    z_dir = self.normal_at(origin)
+    return Plane(origin=origin, x_dir=x_dir, z_dir=z_dir).to_location()
+
+
+Face.location = property(_face_location)
