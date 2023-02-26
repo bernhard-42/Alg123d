8706ba7fc756feac2405f00ae043aff9e60242c1
Bernhard <b_walter@arcor.de>
2023-02-26 19:09:30 +0100

    test multiple faces

--- tests/test_lazy.py
+++ tests/test_lazy.py
@@ -1,6 +1,23 @@
 import time
+from cq_vscode import show, show_object, reset_show, set_port, set_defaults, get_defaults
 
 from alg123d import *
+
+
+# %%
+
+diam =  200  # 175
+meshop = 2
+gridxy = int(diam / meshop / 2)
+
+r = Rectangle(meshop, meshop)
+with Copy():
+    holes = [
+        r @ loc
+        for loc in GridLocations(meshop * 2, meshop * 2, gridxy, gridxy)
+        if loc.position.X**2 + loc.position.Y**2 < (diam / 2 - meshop * 0.9) ** 2
+    ]
+show(*holes)
 
 # %%
 diam = 200  # 175
@@ -8,24 +25,28 @@ meshop = 2
 gridxy = int(diam / meshop / 2)
 
 a = time.time()
-with LazyAlgCompound() as holes:
-    r = Rectangle(meshop, meshop)
-    for loc in GridLocations(meshop * 2, meshop * 2, gridxy, gridxy):
-        if loc.position.X**2 + loc.position.Y**2 < (diam / 2 - meshop * 0.9) ** 2:
-            holes += r @ loc
+with Copy():
+    with LazyAlgCompound() as holes:
+        r = Rectangle(meshop, meshop)
+        for loc in GridLocations(meshop * 2, meshop * 2, gridxy, gridxy):
+            if (
+                loc.position.X**2 + loc.position.Y**2
+                < (diam / 2 - meshop * 0.9) ** 2
+            ):
+                holes += r @ loc
 
 c = Circle(diam / 2) - holes
 print(time.time() - a)
 show(c)
-
 # %%
 
 a = time.time()
 
 r = Rectangle(meshop, meshop)
+locs = GridLocations(meshop * 2, meshop * 2, gridxy, gridxy)
 holes = [
     r @ loc
-    for loc in GridLocations(meshop * 2, meshop * 2, gridxy, gridxy)
+    for loc in locs
     if loc.position.X**2 + loc.position.Y**2 < (diam / 2 - meshop * 0.9) ** 2
 ]
 
